d0a503403dfd22c3efae9ce449f303945e852d1e
RoadJDK <realroadjdk@gmail.com>
2022-06-14 22:27:25 +0200

    add auto updater

--- update.py
+++ update.py
@@ -1,14 +1,20 @@
-from shutil import copy2, rmtree
+from os import mkdir
+from shutil import rmtree
 from subprocess import Popen
 from sys import exit
 import urllib.request
 
+rmtree('bot')
+mkdir('bot')
+mkdir('bot/common')
+
 # maybie you could do this automatic:
-urllib.request.urlretrieve('https://raw.githubusercontent.com/RoadJDK/Yuumi-Bot-Extension/main/main.py', 'WTF.py')
-urllib.request.urlretrieve('https://raw.githubusercontent.com/RoadJDK/Yuumi-Bot-Extension/main/main.py', 'WTF.py')
+urllib.request.urlretrieve('https://raw.githubusercontent.com/RoadJDK/Yuumi-Bot-Extension/main/bot/main.py', 'bot/main.py')
+urllib.request.urlretrieve('https://raw.githubusercontent.com/RoadJDK/Yuumi-Bot-Extension/main/bot/requirements.txt', 'bot/requirements.txt')
+urllib.request.urlretrieve('https://raw.githubusercontent.com/RoadJDK/Yuumi-Bot-Extension/main/bot/config.json', 'bot/config.json')
+urllib.request.urlretrieve('https://raw.githubusercontent.com/RoadJDK/Yuumi-Bot-Extension/main/bot/common/champions.json', 'bot/common/champions.json')
+urllib.request.urlretrieve('https://raw.githubusercontent.com/RoadJDK/Yuumi-Bot-Extension/main/bot/common/gamemodes.json', 'bot/common/gamemodes.json')
 
-rmtree('bot') # will delete the folder itself
-
-#Popen("app/main.py", shell=True) # go back to your program
-
+Popen("python bot/main.py", shell=True)
+print()
 exit("exit to restart the true program")
